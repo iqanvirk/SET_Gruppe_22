37d4111cf84495a991152f96ae1730e7cdb70843
Erik H <erik.hanko@hotmail.com>
2024-11-20 17:26:06 +0100

    La til AddPlantScreen som subpage av PlantsScreen
    La til AddPlantScreen som subpage av PlantsScreen, samme som jeg gjorde med filter siden. Dette er for at plante ikonet skal være highlighta mens man er inne på addplant siden

--- app/src/main/java/com/example/myapplication/ui/navigation/navBars/BottomNavBar.kt
+++ app/src/main/java/com/example/myapplication/ui/navigation/navBars/BottomNavBar.kt
@@ -35,7 +35,7 @@ fun BottomNavBar(navController: NavController) {
         shortcuts.forEach { shortcut ->
             val currentScreen = getCurrentScreen(navController) //== shortcut.route.name
             val isSelected = when (shortcut.route) {
-                AppScreens.PLANT -> currentScreen == AppScreens.PLANT.name || currentScreen == AppScreens.FILTER.name // Gjør at filter siden også har planten highlighta, gjør filtersiden til en slags "sub-page" av plantesiden
+                AppScreens.PLANT -> currentScreen == AppScreens.PLANT.name || currentScreen == AppScreens.FILTER.name || currentScreen == AppScreens.ADD_PLANT.name // Gjør at filter og addplant sidene også har planten highlighta, gjør sidene til en slags "sub-page" av plantesiden
                 else -> currentScreen == shortcut.route.name
             }
             NavigationBarItem(
